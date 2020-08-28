e29fe7afc14cf964d8452f1a7494c4a0217ed1e1
Julian Villamil <julianvipe@unisabana.edu.co>
2020-08-28 17:47:35 -0500

    login complete

--- app/src/main/java/Fragments/FirstFragment.kt
+++ app/src/main/java/Fragments/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.example.mypizza
+package Fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,7 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
+import com.example.mypizza.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -24,8 +27,12 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        view.findViewById<Button>(R.id.login).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+
+        view.findViewById<TextView>(R.id.linkCreate).setOnClickListener {
+            findNavController().navigate(R.id.to_create_account)
         }
     }
 }

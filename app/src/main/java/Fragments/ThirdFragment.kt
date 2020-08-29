cca3ba17e8f794b22aaff23fb27a1eadeaac0405
juanheri <juanheri@unisabana.edu.co>
2020-08-29 11:39:54 -0500

    crear cuenta tercer fragmento

--- app/src/main/java/Fragments/ThirdFragment.kt
+++ app/src/main/java/Fragments/ThirdFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.mypizza.R
 
 class ThirdFragment :Fragment()
@@ -16,4 +19,17 @@ class ThirdFragment :Fragment()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_third, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.cancelar_Cuenta).setOnClickListener {
+            findNavController().navigate(R.id.action_ThirdFragment_to_FirstFragment)
+        }
+
+        view.findViewById<TextView>(R.id.crear_Cuenta).setOnClickListener {
+            findNavController().navigate(R.id.action_ThirdFragment_to_SecondFragment)
+        }
+    }
+
 }

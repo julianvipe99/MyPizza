314e7356b687fb2a282d4e1046a3abed3c6ee5d0
Julian Villamil <julianvipe@unisabana.edu.co>
2020-10-01 11:44:56 -0500

    corregi sus partes y agrege un recyclerview

--- app/src/main/java/Fragments/Login.kt
+++ app/src/main/java/Fragments/Login.kt
@@ -5,28 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.example.mypizza.R
+import kotlinx.android.synthetic.main.login.*
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class Login : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.login, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+
+        login.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        linkCreate.setOnClickListener {
+            findNavController().navigate(R.id.to_create_account)
         }
     }
 }
---
+++ app/src/main/java/com/example/mypizza/data/Pizza.kt
@@ -0,0 +1,8 @@
+package com.example.mypizza.data
+
+import android.media.Image
+
+data class Pizza (
+    val id: String,
+    val name: String
+)

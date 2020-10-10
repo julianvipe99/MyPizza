d681348f7dc0a24f5cde1b74f3f7ddc68a724175
camilovegag <camilovegag@unisabana.edu.co>
2020-10-10 08:58:49 -0500

    Fix some front-end stuff

--- app/src/main/java/com/example/mypizza/ui/fragments/Login.kt
+++ app/src/main/java/com/example/mypizza/ui/fragments/Login.kt
@@ -5,29 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.mypizza.R
 import kotlinx.android.synthetic.main.login.*
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class Login : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.login, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         login.setOnClickListener {
+//            Toast.makeText(this, "Iniciando sesión", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+
         }
         linkCreate.setOnClickListener {
             findNavController().navigate(R.id.to_create_account)
